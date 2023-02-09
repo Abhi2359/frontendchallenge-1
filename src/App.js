10c8d84e81cbf866b84528ec0734c0644c6f860c
Abhi2359 <abhishekchoudhary1018@gmail.com>
2023-02-09 21:04:14 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import Layout from "./components/Layout";
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Layout/> 
+    {/*
+      
+    <Card1/>
+    */}
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards/Card1.js
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./Card1.css";
+
+function Card1({ cardNumber, cardHolderName, expMonth, expYear }) {
+  return (
+    <>
+      <div className="card1">
+        <div class="cardinput">
+          <div class="_card__number">
+            <input type="text" value={cardNumber} readOnly id="input" />
+          </div>
+          <div class="_card__details">
+            <input type="text" value={cardHolderName} readOnly id="input" />
+            <div class="expiry">
+              <input type="text" value={expMonth} readOnly id="input" />
+              <div class="slash">/</div>
+              <input type="text" value={expYear} readOnly id="input"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card1;
